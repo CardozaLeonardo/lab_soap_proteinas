7aeaa1ee249166cf5b4448fe0d000cc8f934f5df
Leonardo <leonardoorozco69@gmail.com>
2021-03-28 22:03:57 -0600

    uploading

---
+++ ControlProteinasClient/ControlProteinasForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace ControlProteinasClient
+{
+    partial class ControlProteinasForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.metaField = new System.Windows.Forms.TextBox();
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.addUserButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usersCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.metaLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.addProteinaButton = new System.Windows.Forms.Button();
+            this.totalProteinaField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.metaField);
+            this.groupBox1.Controls.Add(this.nameField);
+            this.groupBox1.Controls.Add(this.addUserButton);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 142);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar nuevo usuario";
+            // 
+            // metaField
+            // 
+            this.metaField.Location = new System.Drawing.Point(61, 62);
+            this.metaField.Name = "metaField";
+            this.metaField.Size = new System.Drawing.Size(114, 20);
+            this.metaField.TabIndex = 4;
+            // 
+            // nameField
+            // 
+            this.nameField.Location = new System.Drawing.Point(61, 32);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(114, 20);
+            this.nameField.TabIndex = 3;
+            // 
+            // addUserButton
+            // 
+            this.addUserButton.Location = new System.Drawing.Point(61, 102);
+            this.addUserButton.Name = "addUserButton";
+            this.addUserButton.Size = new System.Drawing.Size(75, 23);
+            this.addUserButton.TabIndex = 2;
+            this.addUserButton.Text = "Agregar";
+            this.addUserButton.UseVisualStyleBackColor = true;
+            this.addUserButton.Click += new System.EventHandler(this.addUserButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Meta:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombre:";
+            // 
+            // usersCombo
+            // 
+            this.usersCombo.FormattingEnabled = true;
+            this.usersCombo.Location = new System.Drawing.Point(15, 42);
+            this.usersCombo.Name = "usersCombo";
+            this.usersCombo.Size = new System.Drawing.Size(121, 21);
+            this.usersCombo.TabIndex = 1;
+            this.usersCombo.SelectedIndexChanged += new System.EventHandler(this.usersCombo_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccione un usuario";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.metaLabel);
+            this.groupBox2.Controls.Add(this.totalLabel);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.addProteinaButton);
+            this.groupBox2.Controls.Add(this.totalProteinaField);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(261, 87);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 162);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sumar proteina";
+            // 
+            // metaLabel
+            // 
+            this.metaLabel.AutoSize = true;
+            this.metaLabel.Location = new System.Drawing.Point(73, 129);
+            this.metaLabel.Name = "metaLabel";
+            this.metaLabel.Size = new System.Drawing.Size(13, 13);
+            this.metaLabel.TabIndex = 6;
+            this.metaLabel.Text = "0";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(73, 102);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(13, 13);
+            this.totalLabel.TabIndex = 5;
+            this.totalLabel.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Meta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total:";
+            // 
+            // addProteinaButton
+            // 
+            this.addProteinaButton.Location = new System.Drawing.Point(67, 53);
+            this.addProteinaButton.Name = "addProteinaButton";
+            this.addProteinaButton.Size = new System.Drawing.Size(75, 23);
+            this.addProteinaButton.TabIndex = 2;
+            this.addProteinaButton.Text = "Agregar";
+            this.addProteinaButton.UseVisualStyleBackColor = true;
+            this.addProteinaButton.Click += new System.EventHandler(this.OnAddProteina);
+            // 
+            // totalProteinaField
+            // 
+            this.totalProteinaField.Location = new System.Drawing.Point(67, 26);
+            this.totalProteinaField.Name = "totalProteinaField";
+            this.totalProteinaField.Size = new System.Drawing.Size(150, 20);
+            this.totalProteinaField.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Agregar:";
+            // 
+            // ControlProteinasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(505, 282);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usersCombo);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ControlProteinasForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ControlProteinasForm";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox usersCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox totalProteinaField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addProteinaButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox metaField;
+        private System.Windows.Forms.TextBox nameField;
+        private System.Windows.Forms.Button addUserButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label metaLabel;
+        private System.Windows.Forms.Label totalLabel;
+    }
+}
+
---
+++ ControlProteinasClient/ControlProteinasForm.cs
@@ -0,0 +1,53 @@
+﻿using ControlProteinasClient.ServicioControlProteinas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlProteinasClient
+{
+    public partial class ControlProteinasForm : Form
+    {
+        private ProteinTrackingServiceSoapClient service = new ProteinTrackingServiceSoapClient();
+        private User[] users;
+        public ControlProteinasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            users = service.ListUsers();
+            usersCombo.DataSource = users;
+            usersCombo.DisplayMember = "Name";
+            usersCombo.ValueMember = "UserId";
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            service.AddUser(nameField.Text, int.Parse(metaField.Text));
+            users = service.ListUsers();
+            usersCombo.DataSource = users;
+        }
+
+        private void usersCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = usersCombo.SelectedIndex;
+            totalLabel.Text = users[index].Total.ToString();
+            metaLabel.Text = users[index].Goal.ToString();
+        }
+
+        private void OnAddProteina(object sender, EventArgs e)
+        {
+            var userId = users[usersCombo.SelectedIndex].UserId;
+            var newTotal = service.AddProtein(int.Parse(totalProteinaField.Text), userId);
+            users[usersCombo.SelectedIndex].Total = newTotal;
+            totalLabel.Text = newTotal.ToString();
+        }
+    }
+}
